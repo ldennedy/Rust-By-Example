abf869fe01009827aeb079a5436e95fe60ef9c30
ldennedy <laurendennedy@gmail.com>
2023-11-09 01:12:00 -0500

    First Commit

---
+++ hello_world.rs
@@ -0,0 +1,15 @@
+/*
+ * Practicing Rust, using examples from
+ * https://doc.rust-lang.org/rust-by-example/index.html
+ */
+
+fn main() {
+    println!("Hello, World!");
+    println!("I'm a Rustacean!");
+
+    // Example Line Comment
+    /* Example block comment */
+
+    let x = 5 + 90 + 5;
+    println!("Is 'x' 10 or 100? x = {}", x);
+}
